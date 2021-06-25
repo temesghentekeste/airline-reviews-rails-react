6973c2513e0a2d3f0cd452579b2201c2cdceb8d8
Temesghen <mail.temesghen.tekeste@gmail.com>
2021-06-25 19:46:50 +0300

    Add seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,25 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+Airline.create([
+    { 
+      name: "United Airlines",
+      image_url: "https://open-flights.s3.amazonaws.com/United-Airlines.png"
+    }, 
+    { 
+      name: "Southwest",
+      image_url: "https://open-flights.s3.amazonaws.com/Southwest-Airlines.png"
+    },
+    { 
+      name: "Delta",
+      image_url: "https://open-flights.s3.amazonaws.com/Delta.png" 
+    }, 
+    { 
+      name: "Alaska Airlines",
+      image_url: "https://open-flights.s3.amazonaws.com/Alaska-Airlines.png" 
+    }, 
+    { 
+      name: "JetBlue",
+      image_url: "https://open-flights.s3.amazonaws.com/JetBlue.png" 
+    }, 
+    { 
+      name: "American Airlines",
+      image_url: "https://open-flights.s3.amazonaws.com/American-Airlines.png" 
+    }
