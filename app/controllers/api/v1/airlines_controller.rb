197595c3a2428c63eada25851e4532bc8f8b7df8
Temesghen <mail.temesghen.tekeste@gmail.com>
2021-06-25 21:34:52 +0300

    Create api

---
+++ app/controllers/api/v1/airlines_controller.rb
@@ -0,0 +1,68 @@
+module Api
+    module v1
+        class AirlinesController < ApplicationController
+             # GET /api/v1/airlines
+            def index 
+                render json: AirlineSerializer.new(airlines, options).serialized_json
+            end
+
+            # GET /api/v1/airlines/:slug
+            def show 
+                render json: AirlineSerializer.new(airline, options).serialized_json
+            end
+
+            # POST /api/v1/airlines
+            def create 
+                airline = Airline.new(airline_params)
+
+                if airline.save
+                    render json: AirlineSerializer.new(airline).serialized_json
+                else
+                    render json: { error: airline.errors.messages}, status: 422
+                end
+            end
+
+            # PATCH /api/v1/airlines/:slug
+            def update
+
+                if airline.update(airline_params)
+                    render json: AirlineSerializer.new(airline, options).serialized_json
+                else
+                    render json: { error: airline.errors.messages}, status: 422
+                end
+
+            end
+
+            # DELETE /api/v1/airlines/:slug
+            def destroy
+
+                if airline.destroy
+                    head :no_content
+                else
+                    render json: { error: airline.errors.messages}, status: 422
+                end
+                
+            end
+
+            private 
+
+            def airline_params
+                params.require(:ariline).permit(:name, :image_url)
+            end
+
+            def options
+                @options ||= { include: %i[reviews]}
+            end
+
+            # Get all airlines
+            def airlines
+                @airlines ||= Airline.all
+            end
+
+            # Get a specific airline
+            def airline
+                @airline ||= Airline.find_by(slug: params[:slug])
+            end
+        end
+    end
+end
---
+++ app/controllers/api/v1/reviews_controller.rb
@@ -0,0 +1,32 @@
+module Api
+    module v1
+        class ReviewsController < ApplicationController
+
+            def create 
+                review = Review.new(reviews_params)
+
+                if review.save
+                    render json: ReviewSerializer.new(review).serialized_json
+                else
+                    render json: { error: review.errors.messages }, staus: 422
+                end
+            end
+
+            def destroy 
+                review = Review.find(params[:id])
+
+                if review.destroy
+                    head :no_content
+                else
+                    render json: { error: review.errors.messages }, staus: 422
+                end
+            end
+            
+            private 
+
+            def reviews_params
+                params.require(:reviews).permit(:title, :description, :score, :airline_id)
+            end
+        end
+    end
+end
