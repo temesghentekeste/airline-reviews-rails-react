9dddfe2dd88754f8e74cfba6e6d7e9ccbc687b50
Temesghen <mail.temesghen.tekeste@gmail.com>
2021-06-26 02:10:17 +0300

    Create Airlines component; add Airline comp.; render airlines; add axios

--- app/javascript/components/Airlines/index.js
+++ app/javascript/components/Airlines/index.js
@@ -1,7 +1,29 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 
 const Airlines = () => {
-  return <div>Airlines</div>;
+  const [airlines, setAirlines] = useState([]);
+  useEffect(() => {
+    axios
+      .get('/api/v1/airlines.json')
+      .then((res) => setAirlines(res.data.data))
+      .catch((error) => console.log(error));
+  }, []);
+
+  const list = airlines.map((item) => (
+    <li key={item.attributes.name}>{item.attributes.name}</li>
+  ));
+  return (
+    <div className="home">
+      <div className="header">
+        <h1>OpenFlights</h1>
+        <p>Honest, unbieased airline reviews</p>
+      </div>
+      <div className="grid">
+        <ul>{list}</ul>
+      </div>
+    </div>
+  );
 };
 
 export default Airlines;
--- app/javascript/components/App.js
+++ app/javascript/components/App.js
@@ -4,7 +4,6 @@ import Routes from './Routes';
 const App = () => {
   return (
     <div>
-      App
       <Routes />
     </div>
   );
