b5a193e74ef680b2b8341830e47bf716c4058ded
Temesghen <mail.temesghen.tekeste@gmail.com>
2021-06-26 11:38:15 +0300

    Update style of Airlines and Airline comp.

--- app/javascript/components/Airline/index.js
+++ app/javascript/components/Airline/index.js
@@ -1,19 +1,59 @@
 import React from 'react';
-import { Link } from 'react-router-dom'
+import { Link } from 'react-router-dom';
+import styled from 'styled-components';
+
+const Card = styled.div`
+  border: 1px solid #efefef;
+  background: #fff;
+`;
+const AirlineLogo = styled.div`
+  width: 50px;
+  margin: 0 auto;
+  padding: 18px;
+
+  img {
+    height: 50px;
+    width: 50px;
+    border-radius: 100%;
+    border: 1px solid #efefef;
+  }
+`;
+
+const AirlineName = styled.div`
+  padding: 20px 0 10px 0;
+`;
+
+const LinkWrapper = styled.div`
+  margin: 30px 0 20 px 0;
+  height: 50px;
+
+  a {
+    color: #fff;
+    background: #000;
+    border-radius: 4px;
+    padding: 10px 50px;
+    border: 1px solid #000;
+    width: 100%;
+    text-decoration: none;
+    cursor: pointer;
+  }
+`;
 
 const Airline = (props) => {
-    const { image_url, name, avg_score, slug} = props.attributes;
+  const { image_url, name, avg_score, slug } = props.attributes;
   return (
-    <div className="card">
-      <div className="airline-log">
+    <Card>
+      <AirlineLogo>
         <img src={image_url} alt={name} />
-      </div>
-      <div className="airline-name">{name }</div>
+      </AirlineLogo>
+      <div className="airline-name">{name}</div>
       <div className="airline-score">{avg_score}</div>
-      <div className="airline-link">
-          <Link to={`/airlines/${slug}`}>View Airline</Link>
-      </div>
-    </div>
+      <AirlineName>
+        <LinkWrapper>
+          <a to={`/airlines/${slug}`}>View Airline</a>
+        </LinkWrapper>
+      </AirlineName>
+    </Card>
   );
 };
 
--- app/javascript/components/Airlines/index.js
+++ app/javascript/components/Airlines/index.js
@@ -1,5 +1,35 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
+
+import styled from 'styled-components'
+
+
+
+const Home = styled.div`
+    text-align: center;
+    max-width: 1200px;
+    margin: 0 auto;
+
+`
+const Header = styled.div`
+    padding: 100px 100px 10px 100px;
+
+    h1 {
+        font-size: 42px;
+    }
+`
+const SubHeader = styled.div`
+    font-weight: 300px;
+    font-size: 26px;
+    `
+const Grid = styled.div`
+    display: grid;
+    grid-template-columns: repeat(4, 1fr);
+    grid-gap: 20px;
+    width: 100%;
+    padding: 20px;
+`
+
 import Airline from '../Airline';
 
 const Airlines = () => {
@@ -15,13 +45,13 @@ const Airlines = () => {
     <Airline key={item.attributes.name} attributes={item.attributes} />
   ));
   return (
-    <div className="home">
-      <div className="header">
+    <Home>
+      <Header>
         <h1>OpenFlights</h1>
-        <p>Honest, unbieased airline reviews</p>
-      </div>
-      <div className="grid">{grid}</div>
-    </div>
+        <SubHeader>Honest, unbieased airline reviews</SubHeader>
+      </Header>
+      <Grid>{grid}</Grid>
+    </Home>
   );
 };
 
