6a45a31c721ea3e2c229f00ccebe7661f4494c8b
Temesghen <mail.temesghen.tekeste@gmail.com>
2021-06-27 00:34:08 +0300

    Fetch airline data & store it inside a local state

--- app/javascript/components/Airline/index.js
+++ app/javascript/components/Airline/index.js
@@ -1,6 +1,26 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 
-const Airline = () => {
+import axios from 'axios';
+
+const Airline = (props) => {
+  const [airline, setAirline] = useState({});
+  const [review, setReview] = useState({});
+
+  useEffect(() => {
+    // /api/v1/airlines/american-airline
+    // /api/v1/airlines/delta
+
+    const slug = props.match.params.slug;
+    const url = `/api/v1/airlines/${slug}`;
+
+    axios
+      .get(url)
+      .then((res) => setAirline(res.data))
+      .catch((error) => console.log(error));
+  }, []);
+
+  console.log(airline)
+
   return <div>Airline component</div>;
 };
 
