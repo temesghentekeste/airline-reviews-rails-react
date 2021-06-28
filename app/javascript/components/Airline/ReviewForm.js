e61960282acbe0d58dcf40c632b22da0a0bf80e6
Temesghen <mail.temesghen.tekeste@gmail.com>
2021-06-28 13:27:47 +0300

    Update style of review form

--- app/javascript/components/Airline/ReviewForm.js
+++ app/javascript/components/Airline/ReviewForm.js
@@ -12,7 +12,6 @@ const RatingContianer = styled.div`
   border: 1px solid #e6e6e7;
   margin: 20px 0;
   padding: 20px;
-  background: white;
 `;
 
 const RatingBox = styled.div`
@@ -22,7 +21,9 @@ const RatingBox = styled.div`
   overflow: hidden;
   flex-direction: row-reverse;
   position: relative;
-  input { display: none; }
+  input {
+    display: none;
+  }
   label {
     cursor: pointer;
     width: 40px;
@@ -32,16 +33,77 @@ const RatingBox = styled.div`
     background-repeat: no-repeat;
     background-position: center;
     background-size: 76%;
-    transition: .3s;
+    transition: 0.3s;
   }
-  input:checked ~ label, input:checked ~ label ~ label {
+  input:checked ~ label,
+  input:checked ~ label ~ label {
     background-image: url("data:image/svg+xml;charset=UTF-8,%3csvg xmlns='http://www.w3.org/2000/svg' width='126.729' height='126.73'%3e%3cpath fill='%23fcd93a' d='M121.215 44.212l-34.899-3.3c-2.2-.2-4.101-1.6-5-3.7l-12.5-30.3c-2-5-9.101-5-11.101 0l-12.4 30.3c-.8 2.1-2.8 3.5-5 3.7l-34.9 3.3c-5.2.5-7.3 7-3.4 10.5l26.3 23.1c1.7 1.5 2.4 3.7 1.9 5.9l-7.9 32.399c-1.2 5.101 4.3 9.3 8.9 6.601l29.1-17.101c1.9-1.1 4.2-1.1 6.1 0l29.101 17.101c4.6 2.699 10.1-1.4 8.899-6.601l-7.8-32.399c-.5-2.2.2-4.4 1.9-5.9l26.3-23.1c3.8-3.5 1.6-10-3.6-10.5z'/%3e%3c/svg%3e");
   }
   input:not(:checked) ~ label:hover,
   input:not(:checked) ~ label:hover ~ label {
     background-image: url("data:image/svg+xml;charset=UTF-8,%3csvg xmlns='http://www.w3.org/2000/svg' width='126.729' height='126.73'%3e%3cpath fill='%23d8b11e' d='M121.215 44.212l-34.899-3.3c-2.2-.2-4.101-1.6-5-3.7l-12.5-30.3c-2-5-9.101-5-11.101 0l-12.4 30.3c-.8 2.1-2.8 3.5-5 3.7l-34.9 3.3c-5.2.5-7.3 7-3.4 10.5l26.3 23.1c1.7 1.5 2.4 3.7 1.9 5.9l-7.9 32.399c-1.2 5.101 4.3 9.3 8.9 6.601l29.1-17.101c1.9-1.1 4.2-1.1 6.1 0l29.101 17.101c4.6 2.699 10.1-1.4 8.899-6.601l-7.8-32.399c-.5-2.2.2-4.4 1.9-5.9l26.3-23.1c3.8-3.5 1.6-10-3.6-10.5z'/%3e%3c/svg%3e");
   }
-`
+`;
+
+const Field = styled.div`
+  border-radius: 4px;
+  input {
+    width: 96%;
+    min-height: 50px;
+    border-radius: 4px;
+    border: 1px solid #e6e6e6;
+    margin: 12px 0;
+    padding: 12px;
+  }
+
+  textarea {
+    width: 100%;
+    min-height: 80px;
+    border-radius: 4px;
+    border: 1px solid #e6e6e6;
+    margin: 12px 0;
+    padding: 12px;
+  }
+`;
+
+const SubmitBtn = styled.button`
+  color: #fff;
+  background-color: #71b406;
+  border-radius: 4px;
+  padding: 12px 12px;
+  border: 1px solid #71b406;
+  width: 100%;
+  font-size: 18px;
+  cursor: pointer;
+  transition: ease-in-out 0.2s;
+  &:hover {
+    background: #71b406;
+    border-color: #71b406;
+  }
+`;
+
+const ReviewWrapper = styled.div`
+  background: white;
+  padding: 20px;
+  margin-left: 15px;
+  border-radius: 0;
+  padding-bottom: 80px;
+  border-left: 1px solid rgba(0, 0, 0, 0.1);
+  height: 100vh;
+  padding-top: 100px;
+`;
+
+const ReviewHeadline = styled.div`
+  font-size: 20px;
+  padding: 15px 0;
+  font-weight: bold;
+`;
+
+const RatingBoxTitle = styled.div`
+  font-size: 20px;
+  padding-bottom: 20px;
+  font-weight: bold;
+`;
 
 const RatingTitle = styled.div``;
 
@@ -63,12 +125,12 @@ const ReviewForm = (props) => {
   });
 
   return (
-    <div className="wrapper">
+    <ReviewWrapper>
       <form onSubmit={props.handleSubmit}>
-        <div>
+        <ReviewHeadline>
           Have an experience with {props.attributes.name}]? Share your review!
-        </div>
-        <div className="field">
+        </ReviewHeadline>
+        <Field>
           <input
             onChange={props.handleChange}
             value={props.review.title || ''}
@@ -76,9 +138,9 @@ const ReviewForm = (props) => {
             name="title"
             placeholder="Review Title"
           />
-        </div>
+        </Field>
 
-        <div className="field">
+        <Field>
           <input
             onChange={props.handleChange}
             value={props.review.description || ''}
@@ -86,17 +148,17 @@ const ReviewForm = (props) => {
             name="description"
             placeholder="Review Description"
           />
-        </div>
+        </Field>
 
-        <div className="field">
+        <Field>
           <RatingContianer>
-            <div className="rating-title-text">Rate This Airline</div>
+            <RatingTitle>Rate This Airline</RatingTitle>
             <RatingBox>{ratingOptions}</RatingBox>
           </RatingContianer>
-        </div>
-        <button type="submit">Submit Your Review</button>
+        </Field>
+        <SubmitBtn type="submit">Submit Your Review</SubmitBtn>
       </form>
-    </div>
+    </ReviewWrapper>
   );
 };
 
