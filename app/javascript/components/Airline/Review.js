9a9318422f6a7c307a039be1ced52cb524d5a148
Temesghen <mail.temesghen.tekeste@gmail.com>
2021-06-28 16:34:31 +0300

    Add Rating comp.; render rating in Airline comp.

--- app/javascript/components/Airline/Review.js
+++ app/javascript/components/Airline/Review.js
@@ -1,16 +1,57 @@
 import React from 'react';
+import styled from 'styled-components';
+import Rating from '../Rating';
+
+const Card = styled.div`
+  border-radius: 4px;
+  border: 1px solid #e6e6e6;
+  padding: 20px;
+  margin: 0px 0px 20px 0px;
+  position: relative;
+`;
+
+const RatingContianer = styled.div`
+  display: flex;
+`;
+
+const Title = styled.div`
+  padding: 20px 0px 3px;
+  font-weight: bold;
+  font-size: 18px;
+`;
+
+const Description = styled.div`
+  padding: 0 0 20px 0;
+  font-size: 14px;
+`;
+const Options = styled.div`
+  position: absolute;
+  right: 15px;
+  top: 15px;
+  display: flex;
+  flex-direction: columns;
+`;
+
+const Icon = styled.button`
+  box-shadow: none;
+  border-radius: 4px;
+  margin: 0 4px;
+  i {
+    font-size: 18px;
+  }
+`;
 
 const Review = (props) => {
   const { score, title, description } = props.attributes;
   return (
-    <div className="card">
-      <div className="rating-container">
-        <div className="rating-score"> {score}</div>
-
-        <div className="title">{title}</div>
-        <div className="description">{description}</div>
-      </div>
-    </div>
+    <Card>
+      <RatingContianer>
+        <div className="score">{score}</div>
+      </RatingContianer>
+      <Rating score={score} />
+      <Title>{title}</Title>
+      <Description>{description}</Description>
+    </Card>
   );
 };
 
---
+++ app/javascript/components/Rating/index.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import './Rating.css';
+
+const Rating = (props) => {
+  if (!props) {
+    return '<div></div>';
+  }
+
+  const score = props.score;
+
+  const stars = [1, 2, 3, 4, 5].map((item, index) => {
+    if (item <= score) {
+      return <i className="fa fa-star rating" key={index}></i>;
+    } else {
+      return <i className="fa fa-star" key={index}></i>;
+    }
+  });
+  return <div>{stars}</div>;
+};
+
+export default Rating;
